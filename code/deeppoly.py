7ee8012063ff825372cfc1918b3633f06385f309
Garima Singh <garimasingh0028@gmail.com>
2023-12-06 14:40:53 +0100

    Add relu.

--- code/deeppoly.py
+++ code/deeppoly.py
@@ -21,7 +21,7 @@ class DpBounds:
         self.ub = ub
         assert self.lb.shape == self.ub.shape
         assert (self.lb > self.ub).sum() == 0
-        
+
 class DpInput():
 
     def __init__(self, bounds: DpBounds):
@@ -42,17 +42,6 @@ class DpLinear():
         lb = self.constraints.lr @ bounds.lb + self.constraints.lo
         ub = self.constraints.ur @ bounds.ub + self.constraints.uo
         self.bounds = DpBounds(lb, ub)
-    
-    def check_postcondition(self, y) -> bool:
-        try:
-            target = y.item()
-        except AttributeError:
-            target = y
-        target_lb = self.bounds.lb[0][target].item()
-        for i in range(self.bounds.ub.shape[-1]):
-            if i != target and self.bounds.ub[0][i] >= target_lb:
-                return False
-        return True
 
 class DpFlatten():
     def __init__(self, layer : nn.Flatten):
@@ -66,9 +55,47 @@ class DpFlatten():
 class DpRelu():
     def __init__(self, layer : nn.ReLU):
         self.layer = layer
+        self.bounds = None
+        self.slope = None
+        self.bias_upper = None
+        self.constraints = None
+
+    def compute_constraints(self, bounds: DpBounds):
+        self.slope = bounds.ub / (bounds.ub - bounds.lb)
+        self.bias_upper = - bounds.lb * self.slope
+
+        #  0 >= ub >= lb
+        mask_upper = torch.where(bounds.ub <= 0, 0, 1)
+        # ub >= lb >= 0
+        mask_lower = torch.where(bounds.lb >= 0, 1, 0)
+        # ub >= 0 >= lb
+        mask_crossing = 1 - (mask_lower + mask_upper)
+
+        self.slope = mask_crossing * self.slope
+        ur = self.slope
+
+        uo = torch.zeros(bounds.lb.shape)
+        uo = mask_lower * bounds.ub + mask_crossing * self.bias_upper
+
+        # For now use the x >= 0 constraint for lower relu
+        lr = torch.zeros(bounds.lb.shape)
+        lo = mask_lower * bounds.lb
+
+        self.constraints = DpConstraints(lr, ur, lo, uo)
 
     def compute_bound(self, bounds: DpBounds):
-        return NotImplementedError()
+
+        self.compute_constraints(bounds)
+
+        pos_lr = nn.functional.relu(self.constraints.lr)
+        neg_lr = -nn.functional.relu(-self.constraints.lr)
+        lb = pos_lr * bounds.lb + neg_lr * bounds.ub + self.constraints.lo
+
+        pos_ur = nn.functional.relu(self.constraints.ur)
+        neg_ur = -nn.functional.relu(-self.constraints.ur)
+        ub = pos_ur * bounds.ub + neg_ur * bounds.lb + self.constraints.uo
+
+        self.bounds = DpBounds(lb, ub)
 
 class DpConv():
     def __init__(self, layer : nn.Conv2d):
@@ -103,7 +130,7 @@ def deeppoly_backsub():
     raise NotImplementedError()
 
 def propagate_sample(model, x, eps):
-    
+
     bounds = get_input_bounds(x, eps)
     input_layer = DpInput(bounds)
     dp_layers = [input_layer]
@@ -126,9 +153,9 @@ def certify_sample(model, x, y, eps) -> bool:
     return dp_layers.check_postcondition(y)
 
 if __name__ == "__main__":
-    
+
     def simulate(w):
-        
+
         flatten = nn.Flatten()
         linear1 = nn.Linear(1, 2)
         linear1.weight.data = torch.tensor([[1], [1]], dtype=torch.float)
@@ -140,14 +167,13 @@ if __name__ == "__main__":
         model = nn.Sequential(flatten, linear1, relu1, linear2)
         model.eval()
 
-
         x = torch.tensor([[[0]]])
         eps = 1.0
-        
+
         bounds = get_input_bounds(x, eps)
         input_layer = DpInput(bounds)
         dp_layers = [input_layer]
-        
+
         for i, layer in enumerate(model):
             logger_msg = f'Layer {i} - Type: {type(layer).__name__}'
             if isinstance(layer, nn.Flatten):
