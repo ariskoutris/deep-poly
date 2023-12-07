5e8ab220163b40a43355fa9d5eabc434951b9310
Baidyanath Kundu <kundubaidya99@gmail.com>
2023-12-07 16:17:20 +0100

    Add the sample model

--- code/deeppoly.py
+++ code/deeppoly.py
@@ -280,6 +280,51 @@ if __name__ == "__main__":
     args = parser.parse_args()
     weight = args.weight if args.weight is not None else 2.0
 
+    def sample():
+        """
+        linear = nn.Linear(3, 2)
+        linear.weight.data = torch.tensor([[2, 1, -7], [1, 3, 1]], dtype=torch.float)
+        linear.bias.data = torch.tensor([3, -5], dtype=torch.float)
+        leaky = nn.LeakyReLU(negative_slope=0.5)
+        flatten = nn.Flatten()
+        model = nn.Sequential(linear, leaky, flatten)
+        x = torch.tensor([[[1, 0, 0], [1, 0, 1], [0, 1, 1]]], dtype=torch.float)
+        """
+        # The example we did in class
+        linear1 = nn.Linear(2, 2)
+        linear1.weight.data = torch.tensor([[1, 1], [1, -1]], dtype=torch.float)
+        linear1.bias.data = torch.tensor([0, 0], dtype=torch.float)
+        relu1 = nn.ReLU()
+        linear2 = nn.Linear(2, 2)
+        linear2.weight.data = torch.tensor([[1, 1], [1, -1]], dtype=torch.float)
+        linear2.bias.data = torch.tensor([-0.5, 0], dtype=torch.float)
+        relu2 = nn.ReLU()
+        linear3 = nn.Linear(2, 2)
+        linear3.weight.data = torch.tensor([[-1, 1], [0, 1]], dtype=torch.float)
+        linear3.bias.data = torch.tensor([3, 0], dtype=torch.float)
+        flatten = nn.Flatten()
+        model = nn.Sequential(linear1, relu1, linear2, relu2, linear3)
+        model.eval()
+
+
+        x = torch.tensor([[[0, 0]]], dtype=torch.float)
+        print(x.shape)
+        eps = 1.0
+        alphas = [nn.Parameter(torch.empty((0,))),
+                  nn.Parameter(torch.ones((1, 1, 2)) * 0.0),
+                  nn.Parameter(torch.empty((0,))),
+                  nn.Parameter(torch.ones((1, 1, 2)) * 0.0),
+                  nn.Parameter(torch.empty((0,)))]
+
+        dp_layers = propagate_sample(model, x, eps, min_val=-1, max_val=1)
+
+        print(dp_layers[-1].bounds)
+        if check_postcondition(0, dp_layers[-1].bounds):
+            return True
+        bounds = deeppoly_backsub(dp_layers)
+        print(bounds)
+        return check_postcondition(0, bounds)
+
     def simulate(w):
 
         flatten = nn.Flatten()
@@ -304,14 +349,15 @@ if __name__ == "__main__":
         ub = bounds.ub.flatten()
         return ub[1].item()
 
-    import numpy as np
-    import matplotlib.pyplot as plt
+    # import numpy as np
+    # import matplotlib.pyplot as plt
 
-    w_vals = np.linspace(-3, 5, 500)
-    ub_vals = []
-    for w in w_vals:
-        ub_vals.append(simulate(w))
-    plt.plot(w_vals, ub_vals)
-    plt.show()
+    # w_vals = np.linspace(-3, 5, 500)
+    # ub_vals = []
+    # for w in w_vals:
+    #     ub_vals.append(simulate(w))
+    # plt.plot(w_vals, ub_vals)
+    # plt.show()
     
-    #simulate(weight)
+    # simulate(weight)
+    sample()
